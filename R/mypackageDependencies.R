5460865cf6eb47c4979a28182e8ccff93639995c
salasj1 <alejandrosalas0922@gmail.com>
2024-02-21 21:42:30 -0400

    cambio en el resourcePath de mypackageDependencies.R otra vez

--- R/mypackageDependencies.R
+++ R/mypackageDependencies.R
@@ -1,11 +1,22 @@
 mypackageDependencies <- function() {
+  # Obtén la ruta al directorio 'assets' dentro del paquete
+  assetsPath <- system.file("inst/assets", package = "PaquetePrueba")
+
+  # Asegúrate de que el directorio existe
+  if (!dir.exists(assetsPath)) {
+    stop("No se encontró el directorio de los activos: ", assetsPath)
+  }
+
+  # Agrega el directorio de los activos como una ruta de recursos para que Shiny la utilice
+  shiny::addResourcePath("mypackage-assets-0.1", assetsPath)
+
+  # Devuelve la dependencia HTML
   htmlDependency(
     name = "mypackage-assets-0.1",
     version = "0.1",
     package = "PaquetePrueba",
-    src = "assets",
+    src = assetsPath,
     script = "js/myscript.js",
-    stylesheet = "css/mystyles.css",
-    addResourcePath(prefix = 'mypackage-assets-0.1', directoryPath = system.file("inst/assets", package = "PaquetePrueba"))
+    stylesheet = "css/mystyles.css"
   )
 }
