431c6210968dc96a6f138235162fa64ea72e0318
salasj1 <alejandrosalas0922@gmail.com>
2024-02-21 21:24:27 -0400

    cambio en el resourcePath de mypackageDependencies.R

--- R/mypackageDependencies.R
+++ R/mypackageDependencies.R
@@ -1,11 +1,11 @@
 mypackageDependencies <- function() {
   htmlDependency(
-    name = "mypackage-assets-0.6",
+    name = "mypackage-assets-0.1",
     version = "0.1",
     package = "PaquetePrueba",
     src = "assets",
     script = "js/myscript.js",
     stylesheet = "css/mystyles.css",
-    addResourcePath(prefix = 'mypackage-assets-0.6', directoryPath = 'inst/assets')
+    addResourcePath(prefix = 'mypackage-assets-0.1', directoryPath = system.file("inst/assets", package = "PaquetePrueba"))
   )
 }
