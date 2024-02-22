071200430a62ce0975242de90553699307efffc7
salasj1 <alejandrosalas0922@gmail.com>
2024-02-22 00:13:46 -0400

    arreglo de mypackageDependencies

--- R/mypackageDependencies.R
+++ R/mypackageDependencies.R
@@ -1,21 +1,11 @@
 mypackageDependencies <- function() {
-  # Define la ruta relativa al directorio 'assets'
-  assetsPath <- system.file("inst/assets", package = "PaquetePrueba")
-
-  # Asegúrate de que el directorio existe
-  if (!dir.exists(assetsPath)) {
-    stop("No se encontró el directorio de los activos: ", assetsPath)
-  }
-
-  # Agrega el directorio de los activos como una ruta de recursos para que Shiny la utilice
-  shiny::addResourcePath("mypackage-assets", assetsPath)
 
   # Devuelve la dependencia HTML
   htmlDependency(
     name = "mypackage-assets",
     version = "0.2",
     package = "PaquetePrueba",
-    src = assetsPath,
+    src = "assets",
     script = "js/myscript.js",
     stylesheet = "css/mystyles.css"
   )
