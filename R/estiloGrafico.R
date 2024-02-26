bd0b190fe4505e723f28ea35add2ac5711fa54dd
salasj1 <alejandrosalas0922@gmail.com>
2024-02-26 12:27:24 -0400

    Mas cambios esteticos que puede hacer la funcion aplicaEstilo como modificar el color de las barras y de su contorno, le agregar un titulo y tambien cambiar el nombre a ese titulo y los subtítulos del eje X y Y, al igual que la familia de texto y el tamaño

--- R/estiloGrafico.R
+++ R/estiloGrafico.R
@@ -1,5 +1,14 @@
 library(ggplot2)
 # Define la función aplicaEstilo
-aplicaEstilo <- function(grafico, background_color = 'white') {
-  grafico + theme(plot.background = element_rect(fill = background_color))
+aplicaEstilo <- function(grafico, background_color = 'white', fill_color = 'blue', border_color = 'black', titulo = "", etiqueta_x = "", etiqueta_y = "", posicion_leyenda = "top",
+                         family_title = "sans", size_title = 12, family_axis_x = "sans", size_axis_x = 12, family_axis_y = "sans", size_axis_y = 12) {
+  grafico + geom_histogram(fill = fill_color, color = border_color) +
+    theme(plot.background = element_rect(fill = background_color),
+          plot.title = element_text(hjust = 0.5, family = family_title, size = size_title),
+          legend.position = posicion_leyenda,
+          axis.title.x = element_text(family = family_axis_x, size = size_axis_x),
+          axis.title.y = element_text(family = family_axis_y, size = size_axis_y)) +
+    ggtitle(titulo) +
+    xlab(etiqueta_x) +
+    ylab(etiqueta_y)
 }
